9780dddccc7b6d983b6f61ecf3c67a819adbfc70
NickQwQ-sudo <nick.yu.ming@gmail.com>
2025-07-22 10:41:27 +1200

    14 PBA1
    0

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,6 +1,15 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 30;
+let coreSize = 20;        
+let spikeLength = 70;     
+let colorHue = 400;       
+let numSpikes = 6;        
+let hexRadius = 70;       
+    
+
+
+
 
 
 function setup_wallpaper(pWallpaper) {
@@ -23,3 +32,45 @@ function wallpaper_background() {
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   rect(40 ,40, rect_width, rect_height);
 }
+
+function my_symbol() {
+  push();
+  translate(100, 100); 
+
+  colorMode(HSL);
+  angleMode(DEGREES);
+  strokeWeight(2);
+  noFill();
+
+  
+  stroke(colorHue, 100, 50);
+  beginShape();
+  for (let a = 0; a < 360; a += 60) {
+    let x = cos(a) * hexRadius;
+    let y = sin(a) * hexRadius;
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+
+  
+  stroke(colorHue, 100, 70);
+  for (let i = 0; i < numSpikes; i++) {
+    let angle = i * (360 / numSpikes);
+    let x1 = cos(angle) * (coreSize / 2);
+    let y1 = sin(angle) * (coreSize / 2);
+    let x2 = cos(angle) * spikeLength;
+    let y2 = sin(angle) * spikeLength;
+    line(x1, y1, x2, y2);
+  }
+
+  
+  fill(colorHue, 100, 60);
+  noStroke();
+  ellipse(0, 0, coreSize);
+
+  pop();
+}
+
+
+
+
