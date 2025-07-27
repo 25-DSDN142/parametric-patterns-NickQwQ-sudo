8808a1729df16ef31d88e4f044626470ee69fbd1
NickQwQ-sudo <nick.yu.ming@gmail.com>
2025-07-27 16:45:13 +1200

    1.
    frist

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,7 @@
 //your parameter variables go here!
 let rect_width  = 20;
 let rect_height = 30;
-let coreSize = 20;        
-let spikeLength = 70;     
-let colorHue = 400;       
-let numSpikes = 6;        
-let hexRadius = 70;       
+     
     
 
 
@@ -33,43 +29,55 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   rect(40 ,40, rect_width, rect_height);
 }
 
+// Parameters 
+let motifSize = 120;
+let colors = ['#f25f5c', '#247ba0', '#70c1b3', '#ffe066']; 
+
+function setup_wallpaper(pWallpaper) {
+  pWallpaper.output_mode(DEVELOP_GLYPH); 
+  //pWallpaper.output_mode(GRID_WALLPAPER); 
+
+  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.show_guide(true); 
+
+  pWallpaper.grid_settings.cell_width  = 200;
+  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.row_offset  = 50;
+}
+
+function wallpaper_background() {
+  background('#f6f1e9'); 
+}
+
 function my_symbol() {
   push();
   translate(100, 100); 
+  rotate(45); 
 
-  colorMode(HSL);
-  angleMode(DEGREES);
-  strokeWeight(2);
-  noFill();
-
-  
-  stroke(colorHue, 100, 50);
-  beginShape();
-  for (let a = 0; a < 360; a += 60) {
-    let x = cos(a) * hexRadius;
-    let y = sin(a) * hexRadius;
-    vertex(x, y);
-  }
-  endShape(CLOSE);
-
-  
-  stroke(colorHue, 100, 70);
-  for (let i = 0; i < numSpikes; i++) {
-    let angle = i * (360 / numSpikes);
-    let x1 = cos(angle) * (coreSize / 2);
-    let y1 = sin(angle) * (coreSize / 2);
-    let x2 = cos(angle) * spikeLength;
-    let y2 = sin(angle) * spikeLength;
-    line(x1, y1, x2, y2);
-  }
-
-  
-  fill(colorHue, 100, 60);
-  noStroke();
-  ellipse(0, 0, coreSize);
+ 
+  drawRhombusPart(-30, 0, colors[0]); 
+  drawRhombusPart(30, 0, colors[1]);  
+  drawRhombusPart(0, -30, colors[2]); 
+  drawRhombusPart(0, 30, colors[3]);  
 
   pop();
 }
+
+function drawRhombusPart(x, y, col) {
+  push();
+  translate(x, y);
+  fill(col);
+  noStroke();
+  beginShape();
+  vertex(-30, -60);
+  vertex(30, -60);
+  vertex(60, 60);
+  vertex(0, 60);
+  endShape(CLOSE);
+  pop();
+}
+
+
 
 
 
